0b94addb649f8635b1c79cc84a1e950fd9524385
Aknos <161412826+eltontomiyoshi@users.noreply.github.com>
2025-06-16 17:06:30 +0900

    Edit pool.js to allow connection to render database

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,9 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
+import pg from 'pg';
+const { Pool } = pg;
 
-dotenv.config();
-
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+});
 
 export default pool;
